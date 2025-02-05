f417bc218695a324f100154900a1c8ce6bd9ea3f
37anton <antonin.cavanne@outlook.fr>
2025-02-06 00:33:17 +0100

    debug part-stock.module.ts

--- back/src/infrastructure/frameworks/nestjs/part-stock.module.ts
+++ back/src/infrastructure/frameworks/nestjs/part-stock.module.ts
@@ -1,21 +1,33 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PartStockService } from "../../../application/services/part-stock.service";
-import { PartStockController } from "../../frameworks/nestjs/part-stock.controller";
+import { PartStockController } from "./part-stock.controller";
 import { PartStock } from "../../../domain/entities/part-stock.entity";
-import { PART_STOCK_REPOSITORY } from "../../repositories/part-stock.repository";
+import { Part } from "../../../domain/entities/part.entity";
+import { User } from "../../../domain/entities/user.entity";
+import { Notification } from "../../../domain/entities/notification.entity";
+import { UserModule } from "../nestjs/user.module";
+
+import { PART_STOCK_REPOSITORY } from "../../repositories/part-stock.repository"; // Jeton d'injection
 import { SqlPartStockRepository } from "../../repositories/sql/part-stock.repository";
 import { InMemoryPartStockRepository } from "../../repositories/in-memory/part-stock.repository";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PartStock])],
+  imports: [
+    TypeOrmModule.forFeature([PartStock, Part, User, Notification]), // Ajout des entités
+    UserModule, // 🔹 Vérification que UserModule est bien importé
+  ],
   controllers: [PartStockController],
   providers: [
     PartStockService,
+
+    // Injection dynamique selon l'adaptateur utilisé
     {
       provide: PART_STOCK_REPOSITORY,
-      useClass: process.env.USE_IN_MEMORY === "true" ? InMemoryPartStockRepository : SqlPartStockRepository
-    }
+      useClass: process.env.STORAGE_ADAPTER === "in-memory"
+        ? InMemoryPartStockRepository
+        : SqlPartStockRepository,
+    },
   ],
   exports: [PartStockService, PART_STOCK_REPOSITORY],
 })
