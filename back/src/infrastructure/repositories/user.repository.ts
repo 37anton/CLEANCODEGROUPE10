7e59ea77860c49e0bef80c5104910d26058b9410
Antonin <antonin.cavanne@outlook.fr>
2025-02-08 18:41:52 +0100

    ajout fixtures pour créer 2 companies, 2 concessions, des users, + maj repos et user case

--- back/src/infrastructure/repositories/user.repository.ts
+++ back/src/infrastructure/repositories/user.repository.ts
@@ -3,7 +3,13 @@ import { User } from "../../domain/entities/user.entity";
 export const USER_REPOSITORY = 'USER_REPOSITORY';
 
 export interface UserRepository {
-  createUser(email: string, password: string, role: string, isAdmin: boolean): Promise<any>;
+  createUser(
+    email: string, 
+    password: string, 
+    role: string, 
+    isAdmin: boolean,
+    associations?: { companyId?: string; concessionId?: string; clientId?: string }
+  ): Promise<any>;
   findByEmail(email: string): Promise<any>;
   findById(id: string, relations?: string[]): Promise<any>;
   findByEntity(entityId: string): Promise<any[]>;
