e234f9bd6f329b983b8f977f65c5e38cf003d841
Yassine952 <Yassine95210@hotmail.com>
2025-02-08 01:47:39 +0100

    fix error read incident

--- back/src/interfaces/controllers/incident.controller.ts
+++ back/src/interfaces/controllers/incident.controller.ts
@@ -1,18 +1,26 @@
 // src/infrastructure/interfaces/controllers/incident.controller.ts
-import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request, Get, Param } from '@nestjs/common';
 import { CreateIncidentUseCase } from 'src/application/use-cases/create-incident.use-case';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { CreateIncidentDto } from 'src/application/dto/create-incident.dto';
+import { GetIncidentHistoryUseCase } from 'src/application/use-cases/get-incident-history.use-case';
 
 @Controller('incidents')
 @UseGuards(JwtAuthGuard)
 export class IncidentController {
   constructor(
     private readonly createIncidentUseCase: CreateIncidentUseCase,
+    private readonly getIncidentHistoryUseCase: GetIncidentHistoryUseCase,
+
   ) {}
 
   @Post()
   async create(@Request() req, @Body() data: CreateIncidentDto) {
     return this.createIncidentUseCase.execute(data);
+  }
+
+  @Get('vehicle/:vehicleId')
+  async getHistory(@Param('vehicleId') vehicleId: string) {
+    return this.getIncidentHistoryUseCase.execute(vehicleId);
   }
 }
